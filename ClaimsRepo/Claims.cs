bdc4fdb193d9cf8044ae2bd4a86997fa03408f1e
Ryan <Dibzthedibz@gmail.com>
2021-05-15 20:21:41 -0700

    Fixed my Claims Class to build into repo more easily

--- ClaimsRepo/Claims.cs
+++ ClaimsRepo/Claims.cs
@@ -6,43 +6,23 @@ using System.Threading.Tasks;
 
 namespace ClaimsRepo
 {
-    public class Claim1 : Current
+    public enum ClaimType
     {
-        public int ClaimID => 1;
-        public string Description => "Car Accident On 465";
-        public int ClaimAmount => 400;
-        public DateTime DateOfIncident => new DateTime(2018, 4, 25);
-        public DateTime DateOfClaim => new DateTime(2018, 4, 27);
-        bool IsValid()
-        {
-            if (DateOfClaim.Month > (DateOfIncident.Month + 1))
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-        enum ClaimType
-        {
 
-            Car,
-            Home,
-            Theft
+        Car,
+        Home,
+        Theft
 
-        }
-        
-        
     }
-    public class Claim2 : Current
+
+    public class Claim
     {
-        public int ClaimID => 2;
-        public string Description => "House Fire in Kitchen";
-        public int ClaimAmount => 4000;
-        public DateTime DateOfIncident => new DateTime(2018, 4, 11);
-        public DateTime DateOfClaim => new DateTime(2018, 4, 12);
-        bool IsValid()
+        public int ClaimID { get; set; }
+        public string Description { get; set; }
+        public int ClaimAmount { get; set; }
+    public DateTime DateOfIncident { get; set; }
+public DateTime DateOfClaim { get; set; }
+bool IsValid()
         {
             if (DateOfClaim.Month > (DateOfIncident.Month + 1))
             {
@@ -52,41 +32,7 @@ namespace ClaimsRepo
             {
                 return true;
             }
-        }
-        enum ClaimType
-        {
-
-            Car,
-            Home,
-            Theft
-
-        }
-    }
-    public class Claim3 : Current
-    {
-        public int ClaimID => 3;
-        public string Description => "Stolen Pancakes";
-        public int ClaimAmount => 4;
-        public DateTime DateOfIncident => new DateTime(2018, 4, 27);
-        public DateTime DateOfClaim => new DateTime(2018, 6, 01);
-        bool IsValid()
-        {
-            if (DateOfClaim.Month > (DateOfIncident.Month + 1))
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-        enum ClaimType
-        {
-
-            Car,
-            Home,
-            Theft
-
         }
     }
 }
+    
